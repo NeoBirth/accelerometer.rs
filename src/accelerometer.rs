24ebbcfd34ba9e0d49f8d500c895915f56ff9ffb
joseph <josephbgerber@gmail.com>
2019-12-14 19:12:23 -0500

    Added the accel_norm method to Accelerometer trait

--- src/accelerometer.rs
+++ src/accelerometer.rs
@@ -1,13 +1,17 @@
 //! Traits for reading acceleration measurements from accelerometers
 
 use core::fmt::Debug;
-use micromath::vector::Vector;
+use micromath::vector::{F32x3, Vector};
 
 /// Accelerometers which measure acceleration vectors of type `V`
 pub trait Accelerometer<V: Vector> {
     /// Error type
     type Error: Debug;
 
-    /// Get acceleration vector reading from the accelerometer
-    fn acceleration(&mut self) -> Result<V, Self::Error>;
+    /// Get raw acceleration data from the accelerometer
+    fn accel_raw(&mut self) -> Result<V, Self::Error>;
+
+    /// Get normalized acceleration data from the acclerometer
+    /// Ex {0.0, 5.2, 0.0} - 5.2 G's of acceleration in the y-axis
+    fn accel_norm(&mut self) -> Result<F32x3, Self::Error>;
 }
