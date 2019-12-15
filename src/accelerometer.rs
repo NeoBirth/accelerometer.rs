949933c80b6d32297b3f1571065e6bb9bc6e0ba2
joseph <josephbgerber@gmail.com>
2019-12-14 19:51:04 -0500

    Added sample_rate method to Accelerometer

--- src/accelerometer.rs
+++ src/accelerometer.rs
@@ -11,7 +11,11 @@ pub trait Accelerometer<V: Vector> {
     /// Get raw acceleration data from the accelerometer
     fn accel_raw(&mut self) -> Result<V, Self::Error>;
 
-    /// Get normalized acceleration data from the acclerometer
-    /// Ex {0.0, 5.2, 0.0} - 5.2 G's of acceleration in the y-axis
+    /// Get normalized acceleration data from the accelerometer
+    /// Ex. {0.0, 5.2, 0.0} - 5.2 G's of acceleration in the y-axis
     fn accel_norm(&mut self) -> Result<F32x3, Self::Error>;
+
+    /// Get sample rate of accelerometer data
+    /// Ex. 125.0 - sample rate of 125hz
+    fn sample_rate(&mut self) -> Result<f32, Self::Error>;
 }
